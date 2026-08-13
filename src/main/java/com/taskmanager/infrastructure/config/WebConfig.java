5ca4fb64d79a9811016d850e32624a884c34fd6f
RossiniBre <brenorcarbone@gmail.com>
2026-08-13 18:18:08 -0300

    feat: add Cors for api consume by frontend

--- src/main/java/com/taskmanager/infrastructure/config/WebConfig.java
+++ src/main/java/com/taskmanager/infrastructure/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.taskmanager.infrastructure.config;
 import com.taskmanager.infrastructure.http.AuthenticatedUserArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -19,5 +20,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(authenticatedUserArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
